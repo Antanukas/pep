e49beaf8ff7fe9eb602a50145cae8bfa43e66f77
Antanas Bastys <antanasb@wix.com>
2019-10-10 22:58:04 +0300

    with curved

--- src/main/scala/example/WordSearch.scala
+++ src/main/scala/example/WordSearch.scala
@@ -1,5 +1,7 @@
 package example
 
+import example.algos.KMP
+
 import scala.io.Source
 
 object WordSearch extends App {
@@ -14,15 +16,58 @@ object WordSearch extends App {
       val allPossibleWords = g.map(row => row.sliding(target.length, 1))
       allPossibleWords.collectFirst { case rowWords if rowWords.contains(target) => true }.isDefined
     }
+
     findWord(grid) || findWord(grid.transpose)
+  }
+
+  def solveBig(grid: String, target: String): Boolean = solveBig(grid.split("\n"), target)
+
+  def solveBig(grid: Seq[String], target: String): Boolean = solveBig(grid.map(_.toCharArray).toArray, target.toCharArray)
+
+  def solveBig(grid: Array[Array[Char]], target: Array[Char]): Boolean = {
+    def findWord(g: Array[Array[Char]]): Boolean = g.exists(row => row.containsSlice(target))
+    findWord(grid) || findWord(grid.transpose)
+  }
+
+  def solveKMP(grid: String, target: String): Boolean = solveKMP(grid.split("\n"), target)
+
+  def solveKMP(grid: Seq[String], target: String): Boolean = solveKMP(grid.map(_.toCharArray).toArray, target.toCharArray)
+
+  def solveKMP(grid: Array[Array[Char]], target: Array[Char]): Boolean = {
+    val kmp = new KMP(target)
+    def findWord(g: Array[Array[Char]]): Boolean = g.exists(row => kmp.search(row) != row.length)
+    findWord(grid) || findWord(grid.transpose)
+  }
+
+  def solveCurved(grid: String, target: String): Boolean = solveCurved(grid.split("\n"), target)
+
+  def solveCurved(grid: Seq[String], target: String): Boolean = solveCurved(grid.map(_.toCharArray).toArray, target.toCharArray)
+
+  def solveCurved(grid: Array[Array[Char]], target: Array[Char]): Boolean = {
+
+    def findWord(currentRow: Int, currentTarget: Array[Char], found: Array[Char]): Boolean = {
+      println(currentRow)
+      if (currentRow >= grid.length && !found.sameElements(target)) false
+      else if (found.sameElements(target)) true
+      else if (grid(currentRow).containsSlice(currentTarget)) {
+        val newFound = found ++ currentTarget
+        val newCurrentTarget = target.slice(target.indexOfSlice(newFound) + newFound.length, target.length)
+        findWord(currentRow + 1, newCurrentTarget, newFound)
+      } else if (currentTarget.length > 0) {
+        findWord(currentRow, currentTarget.slice(0, currentTarget.length - 1), found)
+      } else {
+        findWord(currentRow - 1, currentTarget.slice(0, currentTarget.length - 1), found)
+      }
+    }
+    findWord(0, target, Array())
   }
 
   def prettyPrint(grid: Seq[Seq[Char]]): Unit = {
     println(grid.map(_.mkString("")).mkString("\n"))
   }
 
-  assert(
-    solveSimple(
+/*  assert(
+    solveBig(
       """FACI
         |OBQP
         |ANOB
@@ -30,14 +75,54 @@ object WordSearch extends App {
       target = "FOAM") == true
   )
 
+  assert(
+    solveKMP(
+      """FACI
+        |OBQP
+        |ANOB
+        |MASS""".stripMargin,
+      target = "FOAM") == true
+  )*/
+
+  assert(
+    solveCurved(
+      """XFOXX
+        |XFOXX
+        |XXAMX
+        |XXXXX""".stripMargin,
+      target = "FOAM") == true
+  )
+
   val input = Source.fromFile("/Users/antanasb/Code/wix/pep/matrix2000x2000.txt").getLines().toSeq
 
-/*  (1 to 5).foreach(_ => solveSimple(input, target = "B" * 1000))
-  val start = System.currentTimeMillis()
-  assert(
-    solveSimple(
-      input,
-      target = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB") == false
-  )
-  println(s"Large done in ${System.currentTimeMillis() - start}ms")*/
+  solveBig(input, target = "B" * 1000)
+
+  {
+    val start = System.currentTimeMillis()
+    assert(
+      solveBig(
+        input,
+        target = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB") == false
+    )
+    println(s"Large done in ${System.currentTimeMillis() - start}ms")
+  }
+
+  {
+    val start = System.currentTimeMillis()
+    assert(
+      solveKMP(
+        input,
+        target = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB") == false
+    )
+    println(s"Large KMP done in ${System.currentTimeMillis() - start}ms")
+  }
+
+  {
+    val abra = Source.fromFile("/Users/antanasb/Code/wix/pep/abracadabra-matrix.txt").getLines().toSeq
+    assert(
+      solveCurved(
+        abra,
+        target = "rarcdbxbaa") == true
+    )
+  }
 }
