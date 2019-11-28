2fed2e880e04cfcecc8b6fc96e3a7e65012afe39
Antanas Bastys <antanasb@wix.com>
2019-11-29 00:37:40 +0200

    crap solution

--- src/main/scala/example/Puddles.scala
+++ src/main/scala/example/Puddles.scala
@@ -6,20 +6,28 @@ object Puddles extends App {
 
   def isIncreasing(d: Int) = d < 0
 
-  def capacity(arr: Array[Int]): Int = {
+  def trap(height: Array[Int]): Int = {
     var puddleSum = 0
     var i = 1
     var j = 0
 
-    var terrain = 0
-    var lastDirection = arr(0).compareTo(arr(1))
+    val arr = height :+ (0)
+    var lastDirection = 0
     while (i < arr.length - 1) {
       val nextDirection = arr(i).compareTo(arr(i + 1))
       println("e", arr(i))
       if (isIncreasing(lastDirection) && isDecreasing(nextDirection) || arr(j) <= arr(i)) {
-        val length = i - j - 1 // +1 ?
+        val length = i - j // +1 ?
         val lowest = Math.min(arr(i), arr(j))
         val maxWater = length * lowest
+
+        var terrain = 0
+        var k = j
+        while (k < i) {
+          terrain += Math.min(lowest, arr(k))
+          k += 1
+        }
+        println("terrain", terrain)
         println("add to sum", i, j, maxWater, terrain, maxWater - terrain)
         puddleSum += maxWater - terrain
         terrain = 0
@@ -32,7 +40,7 @@ object Puddles extends App {
         }
         j = i
       } else {
-        terrain += arr(i)
+        //terrain += arr(i)
       }
 
       lastDirection = nextDirection
@@ -42,8 +50,10 @@ object Puddles extends App {
     puddleSum
   }
 
-  assert(capacity(Array(0, 3, 0, 2, 0, 3, 0)) == 4)
-  assert(capacity(Array(0, 1, 0, 1, 0, 1, 0)) == 2)
-  assert(capacity(Array(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)) == 6)
-  assert(capacity(Array(0, 3, 2, 1, 2, 3, 0)) == 4)
+  assert(trap(Array(5, 2, 1, 2, 1, 5)) == 14)
+  assert(trap(Array(5, 4, 1, 2)) == 1)
+  assert(trap(Array(3, 0, 2, 0, 3)) == 4)
+  assert(trap(Array(1, 0, 1, 0, 1)) == 2)
+  assert(trap(Array(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)) == 6)
+  assert(trap(Array(3, 2, 1, 2, 3)) == 4)
 }
