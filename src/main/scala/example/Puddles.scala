a4a397a4e85b736f65088ed8b9c031526092cc56
Antanas Bastys <antanasb@wix.com>
2019-11-29 09:49:43 +0200

    O(n)

--- src/main/scala/example/Puddles.scala
+++ src/main/scala/example/Puddles.scala
@@ -3,37 +3,38 @@ package example
 object Puddles extends App {
 
   def trap(height: Array[Int]): Int = {
-    var res = 0
-
+    if (height.size < 2) return 0
     var i = 1
-    while (i < height.length - 1) {
-
-      var left = height(i)
-      var j = 0
-      while (j < i) {
-        left = Math.max(left, height(j))
-        j += 1
-      }
-
-      var right = height(i)
-      j = i + 1
-      while (j < height.length) {
-        right = Math.max(right, height(j))
-        j += 1
-      }
-
-      res = res  + (Math.min(left, right) - height(i))
-      i +=1
+    val leftHighest = new Array[Int](height.length)
+    leftHighest(0) = height(0)
+    while (i < height.length) {
+      leftHighest(i) = Math.max(leftHighest(i - 1), height(i))
+      i += 1
     }
 
-    println(res)
+    val rightHighest = new Array[Int](height.length)
+    rightHighest(height.length - 1) = height(height.length - 1)
+    i = height.length - 2
+    while (i >= 0) {
+      rightHighest(i) = Math.max(rightHighest(i + 1), height(i))
+      i -= 1
+    }
+
+    i = 1
+    var res = 0
+    while (i < height.length - 1) {
+      val maxWater = Math.min(leftHighest(i), rightHighest(i))
+      res = res + (maxWater - height(i))
+      i += 1
+    }
     res
   }
 
-  assert(trap(Array(5, 2, 1, 2, 1, 5)) == 14)
   assert(trap(Array(5, 4, 1, 2)) == 1)
+  assert(trap(Array(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)) == 6)
   assert(trap(Array(3, 0, 2, 0, 3)) == 7)
   assert(trap(Array(1, 0, 1, 0, 1)) == 2)
+  assert(trap(Array(5, 2, 1, 2, 1, 5)) == 14)
   assert(trap(Array(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)) == 6)
   assert(trap(Array(3, 2, 1, 2, 3)) == 4)
 }
